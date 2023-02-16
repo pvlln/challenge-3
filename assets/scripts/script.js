1ce561e1c1888eddcd34a93b683a101b11f0a08e
pvlln <pv823@nyu.edu>
2023-02-16 08:06:53 -0500

    unsuccessful debug

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -24,31 +24,47 @@ function generatePassword(){
       }
       var uppercase = window.confirm("Would you like to include uppercase letters?");
       if (uppercase){
-        elegibleChar += upArray;
+        elegibleChar += ", " + upArray;
       }
       var numeric = window.confirm("Would you like to include numeric characters?");
       if (numeric){
-        elegibleChar += numArray;
+        elegibleChar += ", " + numArray;
       }
       var special = window.confirm("Would you like to include special characters?");
       if (special){
-        elegibleChar += scArray;
+        elegibleChar += ", " + scArray;
       }
+      console.log(elegibleChar); // CONSOLE CHECK
       // Validate that user selected at least one character type
       if (!lowercase && !uppercase && !numeric && !special){
         window.alert("You must select at least one character type. Please try again.");
-      } else{
-        // For loop that chooses random characters up until the password lenght, using
-        // elegible characters
-        for (let i = 0; i < passwordLength; i++){
-          var password = '';
-          password += elegibleChar[Math.random()*passwordLength];
-        }
+        return;
       }
+      // } else{
+      //   // For loop that chooses random characters up until the password lenght, using
+      //   // elegible characters
+      //   for (let i = 0; i < passwordLength; i++){
+      //     var password = '';
+      //     password += elegibleChar[Math.floor(Math.random())*passwordLength];
+      //   }
+      // }
+      // for (var i = 0; i < passwordLength; i++){
+      //   var password = '';
+      //   password += elegibleChar[Math.floor(Math.random()*passwordLength)];
+      // }
+      // console.log(passwordLength, password); // CONSOLE CHECK
+      // return password;
     } else {
       window.alert("You have entered an invalid number of characters. Please try again.")
     }
-
+    for (var i = 0; i < passwordLength; i++){
+      var password = '';
+      password += elegibleChar[Math.floor(Math.random()*passwordLength)];
+      console.log(password, elegibleChar[Math.floor(Math.random()*passwordLength)])
+      //CONSOLE CHECK
+    }
+    console.log(passwordLength, password); // CONSOLE CHECK
+    return password;
 }
 
 // Write password to the #password input
@@ -57,7 +73,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.textContent = password;
 
 }
 
